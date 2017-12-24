b9041a68199a235c5942e8167106259f31a25bb5
rot1024 <aayhrot@gmail.com>
2017-12-24 10:57:29 +0900

    impl main

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,31 @@
-extern crate haken;
 #[macro_use]
 extern crate clap;
+extern crate console;
+extern crate futures;
+extern crate hyper;
+extern crate hyper_tls;
+extern crate select;
+extern crate tokio_core;
 
 mod model;
+mod source;
 mod args;
 mod param;
+mod cli;
 
 fn main() {
     let param = args::parse();
+    let d = source::get_source(param.source.parse().unwrap()).unwrap();
+    let f = cli::formatter::get_formatter(param.formatter.parse().unwrap()).unwrap();
+
+    match d.get_anime_list(&param.cource) {
+        Ok(l) => {
+            let s = cli::selector::show(&l);
+            let result = f.format(&s);
+            println!("{}", result);
+        }
+        Err(e) => {
+            println!("Failed to fetch data: {}", e.description());
+        }
+    }
 }
