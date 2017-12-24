c08ccf6997ced78f340820c02a257ff58b364882
rot1024 <aayhrot@gmail.com>
2017-12-24 10:55:20 +0900

    impl arg parser

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
 extern crate haken;
+#[macro_use]
+extern crate clap;
 
 mod model;
+mod args;
+mod param;
 
 fn main() {
+    let param = args::parse();
 }
---
+++ src/param.rs
@@ -0,0 +1,7 @@
+use model::Cource;
+
+pub struct Param {
+    pub cource: Cource,
+    pub source: String,
+    pub formatter: String,
+}
